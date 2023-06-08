edf81fc3c8fbae7f5d6067dd460ec6a84ab22572
dannyaam9 <dannyaam9@gmail.com>
2023-06-07 19:26:43 -0700

    added helping hand screenshot

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,159 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { Outfit } from 'next/font/google';
+
+import profile from '../public/profile.png'
+
+import flaskIcon from '../public/icons/flask.svg'
+import swiftUIIcon from '../public/icons/swiftui.svg'
+import nextJsIcon from '../public/icons/nextjs.svg'
+import tailwindIcon from '../public/icons/tailwind.svg'
+import mongoDbIcon from '../public/icons/mongodb.svg'
+import flutterIcon from '../public/icons/flutter.svg'
+import python from '../public/icons/python.svg'
+import java from '../public/icons/java.svg'
+import golang from '../public/icons/golang.svg'
+import javascript from '../public/icons/javascript.svg'
+import cppIcon from '../public/icons/cpp.svg'
+
+import helpingHandHomepage from '../public/helping_hand/homepage.png'
+
+
+const outfit = Outfit({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <body className={outfit.className}>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+      <div className='h-24'></div>
+
+      <h1 className='text-center text-5xl tracking-wide'>I am
+        <b> Daniel Amirtharaj</b>
+      </h1>
+
+      <div className='h-8'></div>
+
+      <Image
+        src={profile}
+        width={352}
+        height={352}
+        alt='profile picture'
+        className='mx-auto'
+      />
+
+      <div className='h-8'></div>
+
+      <p className='text-justify leading-loose tracking-wide text-lg max-w-3xl mx-auto text-2xl'>I am a sophomore at Santa Clara University majoring in Computer Science and Engineering. I have experience with several programming languages and frameworks. In my free time, I enjoy weightlifting, hiking, and sneaker reselling.</p>
+
+      <div className='h-16'></div>
+
+      <div className='columns-11 max-w-4xl mx-auto'>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={python}
+          height={40}
+          alt='python icon'
+          className='mx-auto'
+        />
+
+        <Image
+          src={java}
+          height={40}
+          alt='java icon'
+          className='mx-auto'
+        />
+
+        <Image
+          src={golang}
+          height={40}
+          alt='golang icon'
+          className='mx-auto'
+        />
+
+        <Image
+          src={javascript}
+          height={40}
+          alt='javascipt icon'
+          className='mx-auto'
+        />
+
+        <Image
+          src={cppIcon}
+          height={40}
+          alt='mongodb icon'
+          className='mx-auto'
+        />
+
+        <Image
+          src={flaskIcon}
+          height={40}
+          alt='flask icon'
+          className='mx-auto'
+        />
+
+        <Image
+          src={swiftUIIcon}
+          height={40}
+          alt='swiftui icon'
+          className='mx-auto'
+        />
+
+        <Image
+          src={nextJsIcon}
+          height={40}
+          alt='nextjs icon'
+          className='mx-auto'
+        />
+
+        <Image
+          src={tailwindIcon}
+          height={40}
+          alt='reactjs icon'
+          className='mx-auto'
+        />
+
+        <Image
+          src={mongoDbIcon}
+          height={40}
+          alt='mongodb icon'
+          className='mx-auto'
+        />
+
+        <Image
+          src={flutterIcon}
+          height={40}
+          alt='flutter icon'
+          className='mx-auto'
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className='h-40'></div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <div className='columns-2 max-w-5xl mx-auto'>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+        <div>
+          <h1 className='text-5xl tracking-wide font-bold mx-auto'>Helping Hand
+          </h1>
+          <Image
+            src={helpingHandHomepage}
+            width={352}
+            alt='profile picture'
+          />
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className=''>
+          <div className='h-48'></div>
+          <p className='text-justify leading-loose tracking-wide text-lg'>This iOS application provides resources and locations of opioid overdose prevention and where to get Narcan.</p>
+          <ul className='list-disc list-inside text-lg'>
+            <li className='font-bold'>SwiftUI</li>
+            <li className='font-bold'>Auth0</li>
+            <li className='font-bold'>Flask</li>
+            <li className='font-bold'>GPT-3 API</li>
+            <li className='font-bold'>Apple Maps API</li>
+          </ul>
+        </div>
       </div>
-    </main>
+
+      <div className='h-40'></div>
+
+    </body>
   )
 }
