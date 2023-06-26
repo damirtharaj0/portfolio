8ac67d6d6ca24cb41fa037a560e9a8c4484bb979
dannyaam9 <dannyaam9@gmail.com>
2023-06-25 18:17:31 -0700

    added rdt to projects

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,8 +1,9 @@
 
 import ProjectItem from './project-item'
-import helpingHandImg from '../../public/helping_hand/homepage.png';
+import helpingHandImg from '../../public/helping-hand/homepage.png';
 import workoutAppImg from '../../public/workout_app/workout.png'
 import portfolioImg from '../../public/portfolio/portfolio.png'
+import rdtImg from '../../public/rdt-3.0/rdt.png'
 
 const Projects = () => {
     return (
@@ -12,6 +13,7 @@ const Projects = () => {
                     <ProjectItem title='Helping Hand' backgroundImg={helpingHandImg} skills={['SwiftUI', 'Auth0', 'Flask', 'GPT-3 API', 'Apple Maps API']} />
                     <ProjectItem title='Workout App' backgroundImg={workoutAppImg} skills={['Flutter', 'Flask', 'MongoDB']} />
                     <ProjectItem title='Portfolio' backgroundImg={portfolioImg} skills={['Next.js', 'Tailwind CSS', 'Figma']} />
+                    <ProjectItem title='RDT 3.0 implementation' backgroundImg={rdtImg} skills={['C', 'Socket Programming']} />
                 </div>
             </div>
         </div>
