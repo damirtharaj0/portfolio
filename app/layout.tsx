f7966d512dcd8d26362ce691a84747e65f6f7d77
dannyaam9 <dannyaam9@gmail.com>
2023-06-28 17:04:41 -0700

    added description

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Outfit } from 'next/font/google';
 
 export const metadata = {
   title: 'Daniel Amirtharaj',
-  description: 'Portfolio',
+  description: 'I am a junior at Santa Clara University pursuing a B.S. in Computer Science and Engineering. I have experience with several programming languages and frameworks. In my free time, I enjoy weightlifting, hiking, and wrestling.',
 }
 
 const outfit = Outfit({ subsets: ['latin'] });
